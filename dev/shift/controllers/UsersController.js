af2479dd8f538fec8c159d0f8ca50d69835d10c9
Nuoram <antoinemaroun@gmail.com>
2021-03-23 14:14:58 -0400

    Add user reference when creation

--- dev/shift/controllers/UsersController.js
+++ dev/shift/controllers/UsersController.js
@@ -1,14 +1,27 @@
 var User = require('../models/user');
 var bcrypt = require('bcrypt');
+<<<<<<< Updated upstream
+=======
+const { select } = require('async');
+const { body, validationResult } = require('express-validator');
+>>>>>>> Stashed changes
 
 
 
 exports.loginPage = function (req, res, next) {
-    res.render('login',{title:"Connexion",error:"",})
+    res.render('login', { title: "Connexion", error: "", })
 }
 
 exports.homePage = function (req, res, next) {
+<<<<<<< Updated upstream
     res.render('home',{username:req.session.username})
+=======
+    res.render('home', { currentUser })
+}
+
+exports.signupPage = function (req, res, next) {
+    res.render('signup')
+>>>>>>> Stashed changes
 }
 
 
@@ -18,6 +31,7 @@ exports.homePage = function (req, res, next) {
  */
 exports.loginVerif = function (req, res, next) {
     User.
+<<<<<<< Updated upstream
     find().
     where('username').equals(req.body.username).
     select('password username').
@@ -39,10 +53,72 @@ exports.loginVerif = function (req, res, next) {
         
     })
       
+=======
+        find().
+        where('username').equals(req.body.username).
+        select().
+        limit(1).
+        exec(function (err, user) {
+
+            if (user.length == 1) {
+                bcrypt.compare(req.body.password, user[0].password, function (err, result) {
+                    if (result) {
+                        currentUser = user
+                        res.redirect('/home'); // Login info valid
+                    } else {
+                        res.render('login', { title: "Connexion", error: "Mauvais mot de passe", username: user[0].username }); // password invalid
+                    }
+                })
+            } else {
+                res.render('login', { title: "Connexion", error: "Mauvais nom d'utilisateur et mot de passe" }); // Login info invalid
+            }
+
+        })
+
+>>>>>>> Stashed changes
 }
 
 exports.addUser = function (req, res, next) {
+<<<<<<< Updated upstream
     res.send('NOT IMPLEMENTED: addUser: ' + req.params.id);
+=======
+
+    if (req.body.username != [] && req.body.password != []) {
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            bcrypt.hash(req.body.password, salt, function (err, hash) {
+                let user = new User({
+                    username: req.body.username,
+                    password: hash,
+                    isAdmin: false
+                })
+                user.save(function (err) {
+                    if (err) {
+                        res.render('signup', { error: "Nom d'utilisateur est déjà existant" })
+                    }
+                    else {
+                        User.
+                            find().
+                            where('username').equals(req.body.username).
+                            select().
+                            limit(1).
+                            exec(function (err, user) {
+
+                                if (user.length == 1) {
+                                    currentUser = user;
+                                    res.render('home', { currentUser });
+                                }
+                            })
+                    }
+                });
+            })
+        })
+
+    } else {
+        res.render('signup', { error: "Les informations ne sont pas valides" })
+    }
+
+
+>>>>>>> Stashed changes
 }
 
 
--- dev/shift/public/javascripts/login.js
+++ dev/shift/public/javascripts/login.js
@@ -7,7 +7,7 @@ window.addEventListener('load',()=>{
         let username = document.getElementById('username').value;
         let password = document.getElementById('password').value;
         
-        fetch("/director/login/connection",{
+        fetch("/login/connection",{
             method: 'POST',
             body:JSON.stringify({"username":username,"password":password}),
             headers: {"Content-Type": "application/json"}
