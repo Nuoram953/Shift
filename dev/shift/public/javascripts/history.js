d74330765a2734aaa0158ccf3b7d7ae076612066
Nuoram <antoinemaroun@gmail.com>
2021-04-10 12:24:56 -0400

    Added chart for cpm/date

--- dev/shift/public/javascripts/history.js
+++ dev/shift/public/javascripts/history.js
@@ -12,51 +12,34 @@ let ctx = null;
 
 let player = null;
 let games = null;
-let cpm = null;
-let date = null;
+
 
 window.addEventListener("load", () =>{
     canvas = document.getElementById("chart");
     ctx = canvas.getContext("2d");
 
-    ctx.canvas.parentNode.style.width = "400px";
+    ctx.canvas.parentNode.style.width = "600px";
     ctx.canvas.parentNode.style.height = "400px";
 
     player = document.getElementById("player").textContent;
-    cpm = document.getElementById('cpm').textContent.split(",");
-    date = document.getElementById('date').textContent.split(",").forEach(element => element = Date.parse(element));
-
-    console.log(cpm);
-    console.log(date);
+    let cpm = document.getElementById('cpm').textContent.split(",");
+    let dateFormat = [];
+    document.getElementById('date').textContent.split(",").forEach(element => {
+        let temp = new Date(element)
+        dateFormat.push(`${temp.getDay()}/${temp.getMonth()}/${temp.getFullYear()}`)
+    })
 
     var myChart = new Chart(ctx, {
         type: 'line',
         data: {
-            labels: date,
+            labels: dateFormat,
             datasets: [{
-                label: '# of Votes',
+                label: 'CPM pour les dernières parties',
                 data: cpm,
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1,
-                barPercentage: 0.5,
-                barThickness: 6,
-                maxBarThickness: 8,
-                minBarLength: 2,
+                fill: false,
+                borderColor: 'rgb(75, 192, 192)',
+                tension: 0.1
+
             }]
         },
         options: {
@@ -65,9 +48,17 @@ window.addEventListener("load", () =>{
             scales: {
                 y: {
                     beginAtZero: true
+                },
+                x:{
+                    borderWidth:5
+
                 }
-            }
-        }
+
+         
+                
+            },
+            spanGaps:true
+        },
     });
 })
 
