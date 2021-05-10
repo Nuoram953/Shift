35cca517bbc77da1f2ab1cb5b80b7996be2d19f3
Nuoram <antoinemaroun@gmail.com>
2021-05-10 14:07:59 -0400

    Added new styles in css

--- dev/shift/controllers/UsersController.js
+++ dev/shift/controllers/UsersController.js
@@ -25,16 +25,16 @@ exports.loginPage = function (req, res, next) {
 
 exports.homePage = function (req, res, next) {
     User.
-    find().
-    where('username').equals(req.session.user[0]['username']).
-    select().
-    limit(1).
-    exec(function (err, user) {
-        res.render('home', {
-            currentUser: user,
-            title: "Page d'accueuil"
+        find().
+        where('username').equals(req.session.user[0]['username']).
+        select().
+        limit(1).
+        exec(function (err, user) {
+            res.render('home', {
+                currentUser: user,
+                title: "Page d'accueuil"
+            })
         })
-    })
 }
 
 exports.signupPage = function (req, res, next) {
@@ -54,60 +54,118 @@ exports.historyPage = function (req, res, next) {
     async.parallel({
         game: function (callback) {
             Game.
-            find().
-            where('user').equals(req.session.user).
-            select().
-            sort({
-                time: -1
-            }).
-            exec(callback)
+                find().
+                where('user').equals(req.session.user).
+                select().
+                sort({
+                    time: -1
+                }).
+                exec(callback)
         },
         player: function (callback) {
             User.
-            findById(req.params.id).
-            select().
-            exec(callback)
+                findById(req.params.id).
+                select().
+                exec(callback)
 
         },
-        fav: function (callback) {
+        count: function (callback) {
             Game.aggregate([
                 {
-                  '$match': {
-                    'user': mongoose.Types.ObjectId(req.params.id) 
-                  }
-                }, {
-                  '$group': {
-                    '_id': {
-                      'language': '$language'
-                    }, 
-                    'total': {
-                      '$sum': 1
+                    '$match': {
+                        'user': mongoose.Types.ObjectId(req.params.id)
                     }
-                  }
+                }, {
+                    '$group': {
+                        '_id': {
+                            'language': '$language',
+                        },
+                        'total': {
+                            '$sum': 1
+                        }
+                    }
                 }
-              ]).exec(callback)
-   
+            ]).exec(callback)
+
+        },
+        diff: function (callback) {
+            Game.aggregate([
+                {
+                    '$match': {
+                        'user': mongoose.Types.ObjectId(req.params.id)
+                    }
+                }, {
+                    '$group': {
+                        '_id': {
+                            'difficulty': '$difficulty',
+                        },
+                        'total': {
+                            '$sum': 1
+                        }
+                    }
+                }
+            ]).exec(callback)
+
         }
     }, function (err, results) {
         if (err) return next(err);
 
+        console.log(results.diff);
+        //Gather the cpm of the last 6 games in arrays for easier manipulation
         let count = (results.game.length <= 6) ? results.game.length : 6;
         let date = []
         let cpm = []
 
         if (results.game != 0) {
-            console.log(results.game[0].cpm);
             for (let i = 0; i < count; i++) {
                 cpm.push(results.game[i].cpm)
                 date.push(results.game[i].time)
-
             }
         }
 
+        //Find the langauge with the most game played
+        let idx = 0
+        let total = 0
+        let tempLanguage = {
+            value: 0,
+            language:null
+        }
 
-        console.log(results.player.timePlayed);
-        console.log(results.fav);
-        
+        results.count.forEach(lang=>{
+            if (idx == 0){
+                tempLanguage.value = parseInt(lang.total)
+                tempLanguage.language = lang._id.language
+            }
+            else if(parseInt(lang.total) > tempLanguage.value){
+                tempLanguage.value = parseInt(lang.total)
+                tempLanguage.language = lang._id.language
+            }
+            
+            idx++;
+            console.log(parseInt(lang.total));
+            total += parseInt(lang.total)
+        })
+
+
+        idx = 0
+        let tempDifficulty = {
+            value: 0,
+            difficulty:null
+        }
+        results.diff.forEach(lang=>{
+            if (idx == 0){
+                tempDifficulty.value = parseInt(lang.total)
+                tempDifficulty.difficulty = lang._id.difficulty
+            }
+            else if(parseInt(lang.total) > tempDifficulty.value){
+                tempDifficulty.value = parseInt(lang.total)
+                tempDifficulty.difficulty = lang._id.difficulty
+            }
+            
+            idx++;
+        })
+
+
         res.render("history", {
             currentUser: [results.player],
             title: "Historique de parties",
@@ -115,7 +173,10 @@ exports.historyPage = function (req, res, next) {
             player: results.player,
             cpm: cpm,
             date: date,
-            count:results.fav,
+            count: results.count,
+            favorite: tempLanguage.language,
+            total:total,
+            difficulty:tempDifficulty.difficulty
 
 
         });
@@ -134,33 +195,33 @@ exports.historyPage = function (req, res, next) {
  */
 exports.loginVerif = function (req, res, next) {
     User.
-    find().
-    where('username').equals(req.body.username).
-    select().
-    limit(1).
-    exec(function (err, user) {
+        find().
+        where('username').equals(req.body.username).
+        select().
+        limit(1).
+        exec(function (err, user) {
 
-        if (user.length == 1) {
-            bcrypt.compare(req.body.password, user[0].password, function (err, result) {
-                if (result) {
-                    req.session.user = user
-                    res.redirect('/home'); // Login info valid
-                } else {
-                    res.render('login', {
-                        title: "Connexion",
-                        error: ERROR[002],
-                        username: user[0].username
-                    }); // password invalid
-                }
-            })
-        } else {
-            res.render('login', {
-                title: "Connexion",
-                error: ERROR[003]
-            }); // Login info invalid
-        }
+            if (user.length == 1) {
+                bcrypt.compare(req.body.password, user[0].password, function (err, result) {
+                    if (result) {
+                        req.session.user = user
+                        res.redirect('/home'); // Login info valid
+                    } else {
+                        res.render('login', {
+                            title: "Connexion",
+                            error: ERROR[002],
+                            username: user[0].username
+                        }); // password invalid
+                    }
+                })
+            } else {
+                res.render('login', {
+                    title: "Connexion",
+                    error: ERROR[003]
+                }); // Login info invalid
+            }
 
-    })
+        })
 
 }
 
@@ -184,20 +245,20 @@ exports.addUser = function (req, res, next) {
                         })
                     } else {
                         User.
-                        find().
-                        where('username').equals(req.body.username).
-                        select().
-                        limit(1).
-                        exec(function (err, user) {
+                            find().
+                            where('username').equals(req.body.username).
+                            select().
+                            limit(1).
+                            exec(function (err, user) {
 
-                            if (user.length == 1) {
-                                req.session.user = user;
-                                res.render('home', {
-                                    currentUser: req.session.user,
-                                    title: "Page d'accueuil"
-                                });
-                            }
-                        })
+                                if (user.length == 1) {
+                                    req.session.user = user;
+                                    res.render('home', {
+                                        currentUser: req.session.user,
+                                        title: "Page d'accueuil"
+                                    });
+                                }
+                            })
                     }
                 });
             })
